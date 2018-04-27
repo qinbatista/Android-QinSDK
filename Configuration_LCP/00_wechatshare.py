c9054ec763c3e5d39d7937b7fbf5787b6283de93
qinbatista_PC <qinbatista_PC>
2018-04-27 23:32:48 +0800

    测试打完整包

--- Configuration_LCP/00_wechatshare.py
+++ Configuration_LCP/00_wechatshare.py
@@ -2,6 +2,6 @@ import sys, os, platform
 def PythonLocation():
 	return os.path.dirname(os.path.realpath(__file__))
 def main():
-	os.system("python.exe "+PythonLocation()+"\\..\\01_E2WSDK\\anzhi.py Configuration_LCP")     
+	os.system("python.exe "+PythonLocation()+"\\..\\01_E2WSDK\\wechatshare.py Configuration_LCP")     
 if __name__=='__main__':
     main()
--- Configuration_S3/00_jl.py
+++ Configuration_S3/00_jl.py
@@ -2,6 +2,6 @@ import sys, os, platform
 def PythonLocation():
 	return os.path.dirname(os.path.realpath(__file__))
 def main():
-	os.system("python.exe "+PythonLocation()+"\\..\\01_E2WSDK\\anzhi.py Configuration_S3")     
+	os.system("python.exe "+PythonLocation()+"\\..\\01_E2WSDK\\jl.py Configuration_S3")     
 if __name__=='__main__':
     main()
