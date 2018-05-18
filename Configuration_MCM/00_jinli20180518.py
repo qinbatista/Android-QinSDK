8bae5b673bb169e64a4674ec39c542541c3afc47
qinbatista_PC <qinbatista_PC>
2018-05-18 18:52:55 +0800

    合并金立SDK

---
+++ Configuration_MCM/00_jinli20180518.py
@@ -0,0 +1,7 @@
+import sys, os, platform
+def PythonLocation():
+	return os.path.dirname(os.path.realpath(__file__))
+def main():
+	os.system("python.exe "+PythonLocation()+"\\..\\01_E2WSDK\\jinli20180518.py Configuration_MCM")     
+if __name__=='__main__':
+    main()
