719cb522460dc721d10617e2a54e62a7c61dc1bd
qinbatista_PC <qinbatista_PC>
2018-08-14 13:37:14 +0800

    添加小米海外SDK

---
+++ Configuration_IM/00_xmhw20180814.py
@@ -0,0 +1,7 @@
+import sys, os, platform
+def PythonLocation():
+	return os.path.dirname(os.path.realpath(__file__))
+def main():
+	os.system("python.exe "+PythonLocation()+"\\..\\01_E2WSDK\\xmhw20180814.py Configuration_IM")     
+if __name__=='__main__':
+    main()
