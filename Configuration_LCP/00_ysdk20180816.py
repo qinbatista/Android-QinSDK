1d2f5867a75fbb0efc2c265111e4adeef9fdc707
qinbatista_PC <qinbatista_PC>
2018-08-23 20:12:16 +0800

    同步配置文件

---
+++ Configuration_LCP/00_ysdk20180816.py
@@ -0,0 +1,7 @@
+import sys, os, platform
+def PythonLocation():
+	return os.path.dirname(os.path.realpath(__file__))
+def main():
+	os.system("python.exe "+PythonLocation()+"\\..\\01_E2WSDK\\ysdk20180816.py Configuration_LCP")     
+if __name__=='__main__':
+    main()
---
+++ Configuration_S3/00_chel_439920180801.py
@@ -0,0 +1,7 @@
+import sys, os, platform
+def PythonLocation():
+	return os.path.dirname(os.path.realpath(__file__))
+def main():
+	os.system("python.exe "+PythonLocation()+"\\..\\01_E2WSDK\\chel_439920180801.py Configuration_S3")     
+if __name__=='__main__':
+    main()
