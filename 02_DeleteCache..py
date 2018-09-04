ca36b6e85187cfccc49b60994e7b76e2b90384e7
qinbatista_PC <qinbatista_PC>
2018-09-04 11:58:28 +0800

    准备解决多线程打包不能区分是否为多线程问题

--- 02_DeleteCache..py
+++ 02_DeleteCache..py
@@ -3,6 +3,6 @@ def PythonLocation():
 	return os.path.dirname(os.path.realpath(__file__))
 def main():
 	os.chdir(r''+PythonLocation()+"\\01_E2WSDK\\")
-	os.system("00_MultiThread.py")
+	os.system("00_DeleteCache.py")
 if __name__ == '__main__':
     main()
