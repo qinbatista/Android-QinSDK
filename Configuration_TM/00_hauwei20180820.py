917a40471e42998cbdfc08e20fe87cd62a334dbc
qin_mac <qin.batista@.batista@outlook.com>
2018-11-13 19:57:31 +0800

    重新定义命名规范，添加新游戏

--- Configuration_TM/00_hauwei20180820.py
+++
@@ -1,7 +0,0 @@
-import sys, os, platform
-def PythonLocation():
-	return os.path.dirname(os.path.realpath(__file__))
-def main():
-	os.system("python.exe "+PythonLocation()+"\\..\\01_E2WSDK\\hauwei20180820.py Configuration_TM")     
-if __name__=='__main__':
-    main()
